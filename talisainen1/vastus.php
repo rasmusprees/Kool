c1d0d6fd0a1aeb415cf041f37cdb88733eb41952
rasmusprees <rasmus.prees@gmail.com>
2017-04-13 10:42:46 +0300

    asd

---
+++ talisainen1/vastus.php
@@ -0,0 +1,36 @@
+<?php
+
+$text = $_POST['text'];
+
+
+if(isset($_POST['select'])){
+	$option = $_POST['select'];
+	switch ($option) {
+		case 'bigger':
+			echo (strtoupper($text));
+			break;
+		case 'smaller':
+			echo (strtolower($text));
+			break;
+		case 'firstbig':
+			$juppideks = explode(" ", $text);
+			function fb($text)
+			{
+				return(ucfirst(strtolower($text)));
+			}
+			$firstB = array_map("fb", $juppideks);
+			echo implode(" ", $firstB);
+			break;
+	}
+}
+echo "<br>";
+
+
+
+//------tühikutest vaja lahti saada------
+
+$words = explode(" ", $text); //jagab arrayks mu inputi
+echo sizeof($words); // array suurus koos üleliigsete tühikutega
+
+include_once 'string.html';
+?>
